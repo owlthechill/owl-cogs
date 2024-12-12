59cf8892f3876147bada146f3dbc8544025f4396
tarvergrant <tarvergrant@gmail.com>
2024-12-12 13:27:31 -0600

    updated command to a reaction on any command attempted

--- reactions/reactions.py
+++ reactions/reactions.py
@@ -1,11 +1,11 @@
+import discord
 from redbot.core import commands
 
 class ReactionCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I am updated!")
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        # This will trigger for any command attempted
+        await ctx.send(f"Command {ctx.command} was attempted by {ctx.author}.")
