5edcd798c3ca32cd1e6fa5364e4f5fe0cefe809b
tarvergrant <tarvergrant@gmail.com>
2024-12-12 13:19:03 -0600

    testing updates to cogs

--- reactions/reactions.py
+++ reactions/reactions.py
@@ -8,4 +8,4 @@ class ReactionCog(commands.Cog):
     async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send("I am updated!")
